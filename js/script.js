2dfbb233c6f15cc0628b2700bbad2f3996352ccf
fresh1k <s3mpLe@gmail.com>
2022-05-31 15:13:12 +0300

    add slider

--- js/script.js
+++ js/script.js
@@ -1,5 +1,16 @@
+// Burger
 let burger = document.querySelector("#burger-icon");
 let header = document.querySelector("#header");
 burger.onclick = function() {
     header.classList.toggle("menu-open");
 }
+
+
+//Slider
+
+$(document).ready(function(){
+    $('.carousel').slick({
+        arrows: false,
+        dots: true,
+    });
+});
